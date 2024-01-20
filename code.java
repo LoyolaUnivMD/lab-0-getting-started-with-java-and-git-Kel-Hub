8ac07ca58e3a83659eecf1367ede0ea14e165546
kelechi <kelechiopurum2005@gmail.com>
2024-01-19 23:50:17 -0500

    Wrote my code

---
+++ code.java
@@ -0,0 +1,24 @@
+public class code {
+    public static void main(String[] args) {
+        // Programmers:  Kelechi Opurum
+        // Course:  CS 212, Dr. Nweke
+        // Due Date: 1/19/24
+        // Lab Assignment: 0
+        // Problem Statement:  Prints hello world
+        // Input: None
+        // Output: A text
+        // Credits: [Is your code based on an example in the book, in class, or something else?: N/A
+        // Reminder: you should never take code from the Internet or another person
+
+        // I didn't have a partner since I had a hold on my loyola account, i just got registered today.
+
+        System.out.println("||    //");
+        System.out.println("||   //");
+        System.out.println("||  //");
+        System.out.println("|| //");
+        System.out.println("|| \\");
+        System.out.println("||  \\");
+        System.out.println("||   \\");
+        System.out.println("||    \\");
+    }
+}
